bbaf6d65b79d073cf638d888bcfbc0a5f3cb798c
user <user@this.com>
2020-10-22 16:15:36 -0400

    Our proxys have changed to the network nodes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     /*app.use(proxy('/api', { target: 'http://localhost:8080/' }));
     app.use(proxy('/account', { target: 'http://localhost:8081/' }));*/
 
-    app.use(proxy('/api', { target: 'http://172.20.0.2:8080/' }));
-    app.use(proxy('/account', { target: 'http://172.20.0.3:8081/' }));
+    app.use(proxy('/api', { target: 'http://10.109.237.113:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.104.177.185:8081/' }));
 
 };
